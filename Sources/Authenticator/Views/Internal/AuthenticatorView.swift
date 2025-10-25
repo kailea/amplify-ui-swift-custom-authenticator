26df79ff27b8adc1eea76c64522a95855782273c
Amandeep Kaile <a.kaile@bigpond.com>
2025-10-25 22:47:28 +1100

    Remove scrollview from AuthenticatorView

--- Sources/Authenticator/Views/Internal/AuthenticatorView.swift
+++ Sources/Authenticator/Views/Internal/AuthenticatorView.swift
@@ -8,34 +8,31 @@
 import SwiftUI
 
 struct AuthenticatorView<Content: View>: View {
-    @Environment(\.authenticatorTheme) private var theme
-    @Environment(\.authenticatorOptions) private var options
-    private var isBusy: Bool
-    private let content: Content
-
-    init(isBusy: Bool, @ViewBuilder content: () -> Content) {
-        self.isBusy = isBusy
-        self.content = content()
-    }
-
-    var body: some View {
-        ZStack {
-            ScrollView {
-                VStack(spacing: theme.components.authenticator.spacing.vertical) {
-                    content
-                }
-                .padding(theme.components.authenticator.padding/2)
-                .background(theme.components.authenticator.backgroundColor)
-                .cornerRadius(theme.components.authenticator.cornerRadius)
-                .padding(theme.components.authenticator.padding/2)
-            }
-            .blur(radius: isBusy ? options.busyStyle.blurRadius : 0)
-            .disabled(isBusy)
-
-            if isBusy {
-                AnyView(options.busyStyle.content)
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
+	@Environment(\.authenticatorTheme) private var theme
+	@Environment(\.authenticatorOptions) private var options
+	private var isBusy: Bool
+	private let content: Content
+	
+	init(isBusy: Bool, @ViewBuilder content: () -> Content) {
+		self.isBusy = isBusy
+		self.content = content()
+	}
+	
+	var body: some View {
+		ZStack {
+			VStack(spacing: theme.components.authenticator.spacing.vertical) {
+				content
+			}
+			.padding(theme.components.authenticator.padding/2)
+			.background(theme.components.authenticator.backgroundColor)
+			.cornerRadius(theme.components.authenticator.cornerRadius)
+			.padding(theme.components.authenticator.padding/2)
+			.blur(radius: isBusy ? options.busyStyle.blurRadius : 0)
+			.disabled(isBusy)
+			
+			if isBusy {
+				AnyView(options.busyStyle.content)
+			}
+		}
+	}
 }
